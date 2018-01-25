2df3bda758b062a2df108f842505b4e83396f5b3
Ramacho <ramiro.glez96@gmail.com>
2018-01-24 19:59:57 -0700

    forms 24ene18
    aprendimos a usar los formularios

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="./css/estilos.css">
+</head>
+<body>
+	<h1>Login to my APP</h1>
+	<div class="col-md-4 col-md-offset-4 loginDiv">
+		<form>
+			<div class="form-group">
+				<label class="sr-only" for="exampleInputAmount">
+				Your Username</label>
+				<div class="input-group">
+				    <div class="input-group-addon">
+				    <i class="glyphicon glyphicon-user"></i>
+				    </div>
+				    <input type="text" class="form-control" id="exampleInputAmount" placeholder="Username">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="sr-only" for="exampleInputAmount">
+				Your Email</label>
+				<div class="input-group">
+				    <div class="input-group-addon">
+				    <i class="glyphicon glyphicon-envelope"></i>
+				    </div>
+				    <input type="text" class="form-control" id="exampleInputAmount" placeholder="Email">
+				</div>
+			</div>
+		</form>
+	</div>
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	 <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
